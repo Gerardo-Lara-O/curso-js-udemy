29f7e63177d56cba238c4cd39775120b6c39f25b
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2025-03-11 10:58:01 -0600

    Array Methods

--- 12-ArrayMethods/js/01-app.js
+++ 12-ArrayMethods/js/01-app.js
@@ -8,3 +8,41 @@ const carrito = [
     { nombre: 'Teclado', precio: 400 },
     { nombre: 'Celular', precio: 700 },
 ]
+
+// Comprobar si un valor existe en un arreglo
+meses.forEach((mes)=> {
+    if(mes == 'Enero'){
+        console.log("Enero si existe");
+    }
+})
+
+const resultado = meses.includes('Enero')
+console.log(resultado);
+
+
+// Some
+const existe = carrito.some((producto)=>{
+    return producto.nombre === 'Celular'
+})
+
+console.log(existe);
+
+const existe2 = meses.some((mes)=> mes === 'Diciembre')
+console.log(existe2);
+
+
+meses.forEach((mes,indice)=>{
+    if (mes === 'Abril') {
+        console.log(`Encontrado en el indice ${indice}`);
+        
+    }
+})
+
+const indice = meses.findIndex( mes => mes === 'Abril')
+console.log(indice);
+
+
+
+const indice2 = carrito.findIndex( (producto) => producto.nombre === 'Celular')
+console.log(indice2);
+
